f01c7b77cffb1ac37896b0deb3f4d3aaebb15760
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-12-26 17:04:11 +0100

    Add Put endpoint

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -84,5 +84,22 @@ namespace Mango.Services.CouponAPI.Controllers
             }
             return _response;
         }
+        [HttpPut]
+        public ResponseDto Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(obj);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
     }
 }
