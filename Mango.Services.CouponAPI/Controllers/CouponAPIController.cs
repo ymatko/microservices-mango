9036e61ae1285934973da5e54b8bd9364afd2502
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-12-26 17:03:20 +0100

    Add Post endpoint

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -67,5 +67,22 @@ namespace Mango.Services.CouponAPI.Controllers
             }
             return _response;
         }
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Add(obj);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
     }
 }
