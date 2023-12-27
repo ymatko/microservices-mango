bf39574859a4a0d71910860c1d5a56740816ae8e
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-12-27 16:01:27 +0100

    Add SD class

--- Mango.Web/Models/RequestDto.cs
+++ Mango.Web/Models/RequestDto.cs
@@ -1,10 +1,11 @@
 ﻿using System.Runtime.InteropServices.Marshalling;
+using static Mango.Web.Utility.SD;
 
 namespace Mango.Web.Models
 {
     public class RequestDto
     {
-        public string ApiType { get; set; } = "GET";
+        public ApiType ApiType { get; set; } = ApiType.GET;
         public string Url { get; set; }
         public object Data { get; set; }
         public string AccessToken { get; set; }
