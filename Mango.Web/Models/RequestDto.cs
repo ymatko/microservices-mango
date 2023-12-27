6a4f4957336e89a410ea107165072f47ba12f943
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-12-27 15:59:03 +0100

    Add RequestDto

---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public string ApiType { get; set; } = "GET";
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
