cbeed4aa2b281bdd8fa8da5459ab33afff0a1040
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-12-29 08:16:23 +0100

    Add AppDbContext

---
+++ Mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.AuthAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
