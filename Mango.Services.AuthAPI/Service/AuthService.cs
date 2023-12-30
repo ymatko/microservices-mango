6664fea15332622d6714d3789992e7a5de0e00d8
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-12-30 21:14:27 +0100

    Add roles

--- Mango.Services.AuthAPI/Service/AuthService.cs
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -11,14 +11,33 @@ namespace Mango.Services.AuthAPI.Service
         private readonly AppDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
         public AuthService(AppDbContext db, 
             UserManager<ApplicationUser> userManager,
-            RoleManager<IdentityRole> roleManager)
+            RoleManager<IdentityRole> roleManager,
+            IJwtTokenGenerator jwtTokenGenerator)
         {
             _db = db;
             _userManager = userManager;
             _roleManager = roleManager;
+            _jwtTokenGenerator = jwtTokenGenerator;
         }
+
+        public async Task<bool> AssignRole(string email, string roleName)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(u => u.Email.ToLower() == email.ToLower());
+            if(user != null)
+            {
+                if (!_roleManager.RoleExistsAsync(roleName).GetAwaiter().GetResult())
+                {
+                    _roleManager.CreateAsync(new IdentityRole(roleName)).GetAwaiter().GetResult();
+                }
+                await _userManager.AddToRoleAsync(user, roleName);
+                return true;
+            }
+            return false;
+        }
+
         public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
             var user = _db.ApplicationUsers.FirstOrDefault(u=>
@@ -33,6 +52,7 @@ namespace Mango.Services.AuthAPI.Service
                 };
             }
             // generate jwt token
+            var token = _jwtTokenGenerator.GenerateToken(user);
             UserDto userDTO = new()
             {
                 Email = user.Email,
@@ -44,7 +64,7 @@ namespace Mango.Services.AuthAPI.Service
             LoginResponseDto loginResponseDto = new LoginResponseDto()
             {
                 User = userDTO,
-                Token = ""
+                Token = token
             };
             return loginResponseDto;
         }
