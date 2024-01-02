afd047207ffc0c85d70de1044015caa872777959
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-01-02 21:04:26 +0100

    Add AuthService to Web

---
+++ Mango.Web/Service/AuthService.cs
@@ -0,0 +1,44 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+
+namespace Mango.Web.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IBaseService _baseService;
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+        public async Task<ResponseDto?> AssignAsync(RegisterationRequestDto registerationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = registerationRequestDto,
+                Url = SD.CouponAPIBase + "/api/auth/AssignRole"
+            });
+        }
+
+        public async Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = loginRequestDto,
+                Url = SD.CouponAPIBase + "/api/auth/login"
+            });
+        }
+
+        public async Task<ResponseDto?> RegisterAsync(RegisterationRequestDto registerationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = registerationRequestDto,
+                Url = SD.CouponAPIBase + "/api/auth/register"
+            });
+        }
+    }
+}
