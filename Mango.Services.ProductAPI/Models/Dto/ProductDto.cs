c67283aca36ccd40e7e050915304f79e61f573ba
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-01-05 11:55:46 +0100

    Add Product to Db

--- Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
+++ Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
@@ -1,14 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Mango.Services.ProductAPI.Controllers.Models
+﻿namespace Mango.Services.ProductAPI.Models.Dto
 {
-    public class Product
+    public class ProductDto
     {
-        [Key]
         public int ProductId { get; set; }
-        [Required]
         public string Name { get; set; }
-        [Range(1, 1000)]
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
--- Mango.Services.ProductAPI/Models/Product.cs
+++ Mango.Services.ProductAPI/Models/Product.cs
@@ -1,9 +1,14 @@
-﻿namespace Mango.Services.ProductAPI.Controllers.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ProductAPI.Models
 {
-    public class ProductDto
+    public class Product
     {
+        [Key]
         public int ProductId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Range(1, 1000)]
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
