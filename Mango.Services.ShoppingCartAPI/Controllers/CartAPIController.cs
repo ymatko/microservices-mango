7f997f390afadeb5495fba04c127eb2bb9d09a0a
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-01-11 13:07:05 +0100

    Add endpoints: ApplyCoupon, RemoveCoupon

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -49,8 +49,43 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
             }
             return _response;
         }
-
-            [HttpPost("Cartupset")]
+        [HttpPost("ApplyCoupon")]
+        public async Task<Object> ApplyCoupon([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                var cartFromDb = await _db.CartHeaders.FirstAsync(u => u.UserId == cartDto.CartHeader.UserId);
+                cartFromDb.CouponCode = cartDto.CartHeader.CouponCode;
+                _db.CartHeaders.Update(cartFromDb);
+                await _db.SaveChangesAsync();
+                _response.Result = true;
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpPost("RemoveCoupon")]
+        public async Task<Object> RemoveCoupon([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                var cartFromDb = await _db.CartHeaders.FirstAsync(u => u.UserId == cartDto.CartHeader.UserId);
+                cartFromDb.CouponCode = "";
+                _db.CartHeaders.Update(cartFromDb);
+                await _db.SaveChangesAsync();
+                _response.Result = true;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpPost("Cartupset")]
         public async Task<ResponseDto> CartUpsert(CartDto cartDto)
         {
             try
