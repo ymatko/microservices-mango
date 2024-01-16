5b7c47e9c5561547ba8833ec009d1a22fe4a75f5
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-01-16 15:38:54 +0100

    Add RemoveCart

--- Mango.Services.ShoppingCart/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCart/Controllers/CartAPIController.cs
@@ -65,5 +65,32 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
             }
             return _response;
         }
+        [HttpPost("RemoveCart")]
+        public async Task<ResponseDto> RemoveCart(int cartDetailsId)
+        {
+            try
+            {
+                CartDetails cartDetails = _db.CartDetails.First(u => u.CartDetailsId == cartDetailsId);
+
+                int totalCountOfCartItem = _db.CartDetails.Where(u => u.CartHeaderId == cartDetails.CartHeaderId).Count();
+                _db.CartDetails.Remove(cartDetails);
+
+                if(totalCountOfCartItem == 1)
+                {
+                    var cartHeaderToRemove = await _db.CartHeaders.FirstOrDefaultAsync(u => u.CartHeaderId == cartDetails.CartHeaderId);
+
+                    _db.CartHeaders.Remove(cartHeaderToRemove);
+                }
+                await _db.SaveChangesAsync();
+
+                _response.Result = true;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
     }
 }
