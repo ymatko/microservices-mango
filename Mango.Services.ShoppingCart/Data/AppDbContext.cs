5df1d34f6edccd95bfd4f3f26563318abd39b7e7
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-01-16 12:12:38 +0100

    Add basic API setup

---
+++ Mango.Services.ShoppingCart/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ShoppingCartAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CarDetails> CartDetails { get; set; }
+    }
+}
