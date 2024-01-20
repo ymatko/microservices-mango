2e117d36f868d57789befbc180345e963d3d432b
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-01-20 21:45:38 +0100

    Add BackendApiAuthenticationHttpClientHandler

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -41,6 +41,18 @@ namespace Mango.Web.Controllers
             }
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> RemoveCoupon(CartDto cartDto)
+        {
+            cartDto.CartHeader.CouponCode = "";
+            ResponseDto? response = await _cartService.ApplyCouponAsync(cartDto);
+            if (response != null & response.IsSuccess)
+            {
+                TempData["success"] = "Cart updated successfully";
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
 
         private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
         {
