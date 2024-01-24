deee48f4b1d10d8f92ebf00fb89535a56fbef514
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-01-24 12:15:37 +0100

    Add methods to AzureServiceBusConsumer

--- Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
+++ Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -1,8 +1,11 @@
 ﻿using Azure.Messaging.ServiceBus;
+using Mango.Services.EmailAPI.Models.Dto;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace Mango.Services.EmailAPI.Messaging
 {
-    public class AzureServiceBusConsumer
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
     {
         private readonly string serviceBuConnectionString;
         private readonly string emailCartQueue;
@@ -19,6 +22,38 @@ namespace Mango.Services.EmailAPI.Messaging
 
             var client = new ServiceBusClient(serviceBuConnectionString);
             _emailBusProcessor = client.CreateProcessor(emailCartQueue);
+        }
+
+        public async Task Start()
+        {
+            _emailBusProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
+            _emailBusProcessor.ProcessErrorAsync += ErrorHandler;
+        }
+        public async Task Stop()
+        {
+            await _emailBusProcessor.StopProcessingAsync();
+            await _emailBusProcessor.DisposeAsync();
+        }
+        private async Task OnEmailCartRequestReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CartDto objMessage = JsonConvert.DeserializeObject<CartDto>(body);
+            try
+            {
+                await args.CompleteMessageAsync(args.Message);
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
         }
     }
 }
