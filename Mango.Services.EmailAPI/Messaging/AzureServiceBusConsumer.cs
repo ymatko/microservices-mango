9a8a211e98020e921a234ee812fe23158de4e62b
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-01-24 08:39:53 +0100

    Add AzureServiceBusConsumer

---
+++ Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace Mango.Services.EmailAPI.Messaging
+{
+    public class AzureServiceBusConsumer
+    {
+        private readonly string serviceBuConnectionString;
+        private readonly string emailCartQueue;
+        private readonly IConfiguration _configuration;
+
+        private ServiceBusProcessor _emailBusProcessor;
+        public AzureServiceBusConsumer(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            serviceBuConnectionString = _configuration.GetValue<string>("SrviceBusConnectionString");
+
+            emailCartQueue = _configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue");
+
+            var client = new ServiceBusClient(serviceBuConnectionString);
+            _emailBusProcessor = client.CreateProcessor(emailCartQueue);
+        }
+    }
+}
