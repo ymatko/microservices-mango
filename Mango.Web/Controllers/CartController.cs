e487d79ace88772d053710ec04e250a2e0dc7ac3
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-01-26 23:10:09 +0100

    Add payment Intent and Status

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -58,6 +59,15 @@ namespace Mango.Web.Controllers
         [Authorize]
         public async Task<IActionResult> Confirmation(int orderId)
         {
+            ResponseDto? response = await _orderService.ValidateStripeSession(orderId);
+            if (response != null & response.IsSuccess)
+            {
+                OrderHeaderDto orderHeader = JsonConvert.DeserializeObject<OrderHeaderDto>(Convert.ToString(response.Result));
+                if(orderHeader.Status == SD.Status_Approved)
+                {
+                    return View(orderId);
+                }
+            }
             return View(orderId);
         }
         public async Task<IActionResult> Remove(int cartDetailsId)
--- Mango.Web/Utility/SD.cs
+++ Mango.Web/Utility/SD.cs
@@ -19,5 +19,12 @@ namespace Mango.Web.Utility
             PUT,
             DELETE
         }
+
+        public const string Status_Pending = "Pending";
+        public const string Status_Approved = "Approved";
+        public const string Status_ReadyForPickup = "ReadyForPickup";
+        public const string Status_Completed = "Completed";
+        public const string Status_Refunded = "Refunded";
+        public const string Status_Cancelled = "Cancelled";
     }
 }
