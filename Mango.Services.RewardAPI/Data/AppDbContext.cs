d34cb439beb07362b1aafcb3490b76561faf198c
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-01-28 21:17:04 +0100

    Add Db to RewardAPI

---
+++ Mango.Services.RewardAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Services.RewardAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.RewardAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Rewards> Rewards { get; set; }
+    }
+}
