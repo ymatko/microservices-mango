b362a20db926e88ce552cfe35e5d8a837f6b0b5a
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-01-31 21:33:48 +0100

    Add Table of Orders

---
+++ Mango.Web/wwwroot/js/order.js
@@ -0,0 +1,15 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').dataTable({
+        "ajax": { url: "/order/getall" },
+        "columns": [
+            { Data: 'orderheaderid', "width": "5%"}
+            { Data: 'email', "width": "25%"}
+        ]
+    })
+}
