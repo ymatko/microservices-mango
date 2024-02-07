e9c74a30a77b5f04cc8f34053e9f3e5cb75c5897
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-02-07 08:43:03 +0100

    Change HomeUI

--- Mango.Web/Controllers/OrderController.cs
+++ Mango.Web/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Service.IService;
 using Mango.Web.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
@@ -15,10 +16,12 @@ namespace Mango.Web.Controllers
         {
             _orderService = orderService;
         }
+        [Authorize]
         public IActionResult OrderIndex()
         {
             return View();
         }
+        [Authorize]
         public async Task<IActionResult> OrderDetail(int orderId)
         {
             OrderHeaderDto orderHeaderDto = new OrderHeaderDto();
