8a9adebe258cff962a7773d834412589d4d9e3f2
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-02-12 23:10:05 +0100

    Add UserController

---
+++ Mango.Web/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "ADMIN")]
+        [HttpGet]
+        public IActionResult GetAll(string role)
+        {
+            IEnumerable<ApplicationUser> list;
+            ResponseDto response = _userService.GetAllAsync().GetAwaiter().GetResult();
+            string userId = "";
+            if (response.Result != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ApplicationUser>>(Convert.ToString(response.Result));
+                switch (role)
+                {
+                    case "admin":
+                        list = list.Where(u => u.Role == SD.RoleAdmin);
+                        break;
+                    case "customer":
+                        list = list.Where(u => u.Role == SD.RoleCustomer);
+                        break;
+                    default: 
+                        break;
+                }
+            }
+            else
+            {
+                list = new List<ApplicationUser>();
+            }
+            return Json(new { data = list });
+        }
+    }
+}
---
+++ Mango.Web/wwwroot/js/user.js
@@ -0,0 +1,39 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    var url = window.location.search;
+    if (url.includes("admin")) {
+        loadDataTable("admin");
+    }
+    else {
+        if (url.includes("customer")) {
+            loadDataTable("customer");
+        }
+        else {
+            loadDataTable("all");
+        }
+    }
+});
+
+function loadDataTable(status) {
+    dataTable = $('#tblData').DataTable({
+        order: [[0, 'desc']],
+        "ajax": { url: "/user/getall?role=" + role },
+        "columns": [
+            { data: 'Id', "width": "5%" },
+            { data: 'email', "width": "25%" },
+            { data: 'name', "width": "20%" },
+            { data: 'phoneNumber', "width": "20%" },
+            { data: 'role', "width": "10%" },
+            {
+                //data: 'Id',
+                //"render": function (data) {
+                //    return `<div class="w-75 btn-group" role="group">
+                //    <a href="/user/orderDetail?orderId=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i></a>
+                //    </div>`
+                //},
+                //"width": "10%"
+            }
+        ],
+    })
+}
