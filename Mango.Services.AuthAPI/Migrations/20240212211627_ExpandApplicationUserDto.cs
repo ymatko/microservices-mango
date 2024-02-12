bbd8af5e986bbbc8dffed2f5a2b0726508fc9c1d
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-02-12 22:23:15 +0100

    ExpandApplicationUserDto

---
+++ Mango.Services.AuthAPI/Migrations/20240212211627_ExpandApplicationUserDto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.AuthAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExpandApplicationUserDto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "AspNetUsers");
+        }
+    }
+}
--- Mango.Services.AuthAPI/Models/ApplicationUser.cs
+++ Mango.Services.AuthAPI/Models/ApplicationUser.cs
@@ -5,5 +5,6 @@ namespace Mango.Services.AuthAPI.Models
     public class ApplicationUser : IdentityUser
     {
         public string Name { get; set; }
+        public string? Role { get; set; }
     }
 }
